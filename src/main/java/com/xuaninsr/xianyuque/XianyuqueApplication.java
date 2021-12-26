1f58f4059f88381f3115eeb28ac29423725924f9
XuanInsr <2087868649@qq.com>
2021-12-26 23:39:36 +0800

    SpringBoot Start
    Start from https://start.spring.io/

---
+++ src/main/java/com/xuaninsr/xianyuque/XianyuqueApplication.java
@@ -0,0 +1,13 @@
+package com.xuaninsr.xianyuque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XianyuqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XianyuqueApplication.class, args);
+	}
+
+}
