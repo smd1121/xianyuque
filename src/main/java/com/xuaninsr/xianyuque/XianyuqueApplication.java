c8d68b26791bc5301d00ed15b5ad629adf3a5d0d
XuanInsr <2087868649@qq.com>
2021-12-27 13:44:00 +0800

    demo + db dependencies
    可以运行了，配置好了数据库的依赖

--- src/main/java/com/xuaninsr/xianyuque/XianyuqueApplication.java
+++ src/main/java/com/xuaninsr/xianyuque/XianyuqueApplication.java
@@ -2,12 +2,11 @@ package com.xuaninsr.xianyuque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;//新增
+//@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})//修改
 @SpringBootApplication
 public class XianyuqueApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(XianyuqueApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/xuaninsr/xianyuque/controller/xianyuqueController.java
@@ -0,0 +1,32 @@
+package com.xuaninsr.xianyuque.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller // controller 想要一个模板！
+public class xianyuqueController {
+    @RequestMapping("/hello")
+    @ResponseBody   // 将返回的 String 插入到 HTML body 中给 controller
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping("/list2")
+    public String list2(Model model){
+        System.out.println(model);
+        model.addAttribute("hello", "hello world 2");
+        System.out.println(model);
+        // helloThymeleaf 会找到 templates/list.html，总而成为了模板
+        return "list";
+    }
+
+    @RequestMapping("/list")
+    public String list(Model model){
+        System.out.println(model);
+        model.addAttribute("hello", "hello world");
+        System.out.println(model);
+        // helloThymeleaf 会找到 templates/list.html，总而成为了模板
+        return "list";
+    }
+}
